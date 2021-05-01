bb5a1b4dd1cc5de4226a75c95733009480e6cb5b
vishwa795 <visvig66@gmail.com>
2021-05-01 18:10:09 +0530

    Card Made

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react';
 import {Navbar,NavbarBrand,NavbarToggler,Collapse,Nav,NavItem,NavLink,Container,Button,Modal,ModalBody,Row,Col} from 'reactstrap';
-import {AiOutlineLogin} from 'react-icons/ai';
+import {AiOutlineLogin,AiOutlineLogout} from 'react-icons/ai';
+import {TiCancel} from 'react-icons/ti';
 import {useAuth0} from '@auth0/auth0-react';
 function Header(props){
     const [isNavOpen,setIsNavOpen] = React.useState(false);
@@ -8,27 +9,33 @@ function Header(props){
     const domain = process.env.REACT_APP_AUTH0_DOMAIN;
     const [isLoginModalOpen,setIsLoginModalOpen] = React.useState(false);
     const toggleModal = () => setIsLoginModalOpen(!isLoginModalOpen);
-    const {isAuthenticated,loginWithRedirect,user,getAccessTokenSilently} = useAuth0();
+    const {isAuthenticated,loginWithRedirect,user,getAccessTokenSilently,logout} = useAuth0();
     useEffect(()=>{
         const jWTToken = getAccessTokenSilently({
             audience: `https://${domain}/api/v2/`,
             scope: "read:current_user",
           })
-          .then(response=>localStorage.setItem("accessToken",response));
+          .then(response=>localStorage.setItem("accessToken",response))
+          .catch(error=>console.log(error.message));
     })
+    localStorage.setItem("user",user);
     return(
         <div>
-            <Navbar light expand="md" className="navbar">
+            <Navbar light expand="md" className="navbar one-edge-shadow">
                 <Container fluid>
                 <NavbarBrand href="/" className="navbrand"><h2>Plasma Portal</h2></NavbarBrand>
                 <NavbarToggler onClick={toggleNav} />
                 <Collapse isOpen={isNavOpen} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <NavLink href="/components/"><h5>Placeholder link</h5></NavLink>
+                            <Button className="navbutton mr-2" ><TiCancel size="20px" />Deactivate</Button>
                         </NavItem>
                         <NavItem>
-                            <Button className="navbutton-login" onClick={()=>loginWithRedirect()}><AiOutlineLogin size="20px"/><span className="ml-2">Login</span></Button>
+                            {
+                            isAuthenticated?
+                            <Button className="navbutton-login" onClick={() => {logout({ returnTo: window.location.origin })}}><AiOutlineLogout size="20px"/><span >Logout</span></Button>:
+                            <Button className="navbutton-login" onClick={()=>loginWithRedirect()}><AiOutlineLogin size="20px"/><span>Login</span></Button>
+                            }
                         </NavItem>
                     </Nav>
                 </Collapse>
