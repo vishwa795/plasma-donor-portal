0763e41de04c6de3635898395acaa6f65693eb71
Pythonista7 <ashwin.mirskar@gmail.com>
2021-05-02 23:06:09 +0530

    restructuring apollo and auth0
    Signed-off-by: Pythonista7 <ashwin.mirskar@gmail.com>

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -6,10 +6,43 @@ import FAQs from './FAQsComponent'
 import About from './AboutComponent'
 import { useQuery } from '@apollo/client';
 import {GET_USER_DETAILS} from '../Graphql/queries';
+import { withAuth0 } from '@auth0/auth0-react';
 
 import {} from '@auth0/auth0-react';
+import { setContext } from 'apollo-link-context';
+import { ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const httpLink = createHttpLink({
+    uri: 'https://covid-plasma.herokuapp.com/v1/graphql'
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('accessToken');
+    console.log(token);
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      }
+    }
+  });
+  
+  const authdClient = new ApolloClient({
+    link:  authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
+
+  const unauthdClient = new ApolloClient({
+      uri :'https://covid-plasma.herokuapp.com/v1/graphql',
+      cache: new InMemoryCache()
+  })
 
 class Main extends Component{
+    
+      
     constructor(props){
         super(props);
     }
@@ -20,15 +53,17 @@ class Main extends Component{
         return(
             <React.Fragment>
                 <Header />
+                <ApolloProvider client={isAuthenticated?authdClient:unauthdClient}>
                 <Switch location={this.props.location}>
                     <Route path="/about" exact component={(props)=><About {...props} />} />
                     <Route path="/faq" exact component={(props)=><FAQs {...props} />} />
                     <Route path="/:page?" component={(props)=><Home {...props} />} />
                     <Redirect to="/" />
                 </Switch>
+                </ApolloProvider>
             </React.Fragment>
         )
     }
 }
 
-export default withRouter(Main);
+export default withAuth0(withRouter(Main));
--- src/index.js
+++ src/index.js
@@ -4,38 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { setContext } from 'apollo-link-context';
-import { ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
-import { ApolloProvider } from '@apollo/client/react';
-
-const httpLink = createHttpLink({
-  uri: 'https://covid-plasma.herokuapp.com/v1/graphql'
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('accessToken');
-  console.log(token);
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
     <App />
-    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
