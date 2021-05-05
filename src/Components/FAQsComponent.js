f186a0035604afd55664ff52958bd5eb08066445
roy-a2yush <roy.a2yush@gmail.com>
2021-05-06 01:01:38 +0530

    faq beautification

--- src/Components/FAQsComponent.js
+++ src/Components/FAQsComponent.js
@@ -8,21 +8,20 @@ const RenderCards = (props) =>{
         console.log(qa)
         return(
             <Container className="pt-5 pb-2">
-                <Card className="shadow designed-card p3" style={{backgroundColor: '#f8f5f1'}} >
-                    
-                    <CardBody>
-                    <CardTitle className="p3"><h4>{qa.id}. {qa.question}</h4></CardTitle>
-                        <Row>
-                            <Col >
-                                    <div>
-                                    <div className="p1"> <h6>{qa.answer}</h6></div>
-                                    </div>
-                            </Col>
-                            
-                        </Row>
-                    </CardBody>
-                            
-                </Card>
+                <div className="question p-3" style={{backgroundColor: '#f8f5f1', border: 'none', borderLeftStyle: 'solid', borderLeftWidth: '.5rem', borderLeftColor: '#f8a488'}}>
+                    <h1>
+                        <div className='faq-question'>Q.</div>
+                        {qa.question}
+                    </h1>
+                </div>
+                <div className="answer p-3" style={{border: 'none', borderLeftStyle: 'solid', borderLeftWidth: '.5rem', borderLeftColor: '#f8f5f1', color: 'grey'}}>
+                    <div className="faq-answer">
+                        <h1>A.</h1>
+                    </div>
+                    <span>
+                        {qa.answer}
+                    </span>
+                </div>
             </Container>
         )
     })
@@ -32,7 +31,7 @@ export default function FAQs(props){
 
     return <div>
         <Container className="pt-5 pb-2">
-            <h1 style={{color: '#5aa897'}}>FAQs</h1>
+            <h1 style={{color: '#5aa897'}}>Frequently Asked Questions (FAQs)</h1>
         </Container>
         {RenderCards(props)}
     </div>
